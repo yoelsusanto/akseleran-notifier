c11cb02cad4d1b6a7f202369f9847ab67f39b48b
Yoel Susanto <yoelsusantopro@yahoo.com>
2020-12-27 03:32:15 +0700

    Implement config

---
+++ config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type CronConfig struct {
+	Rule string `mapstructure:"rule"`
+}
+
+type DiscordConfig struct {
+	Token     string `mapstructure:"token"`
+	ChannelID string `mapstructure:"channel_id"`
+}
+
+type RedisConfig struct {
+	Address  string `mapstructure:"address"`
+	Password string `mapstructure:"password"`
+	DB       int    `mapstructure:"db_number"`
+}
+
+type ObsScene struct {
+	Name     string `mapstructure:"name"`
+	Image    string `mapstructure:"image"`
+	ButtonId int
+}
+
+type GeneralConfig struct {
+	ServiceName string `mapstructure:"service_name"`
+	Environment string `mapstructure:"environment"`
+}
+
+type Config struct {
+	General       *GeneralConfig         `mapstructure:"general"`
+	RedisConfig   *RedisConfig           `mapstructure:"redis"`
+	DiscordConfig *DiscordConfig         `mapstructure:"discord"`
+	CronConfigs   map[string]*CronConfig `mapstructure:"crons"`
+}
+
+func ReadConfig() (*Config, error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
